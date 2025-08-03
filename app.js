0ed97176dd86c2ac7e2ba2348ba1d292a0cde817
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-03 13:30:37 +0200

    Adding Expanding and Closing Functionality

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 class App {
     constructor() {
-        console.log('app works!')
+        this.$form = document.querySelector('#form');
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$formButtons = document.querySelector('#form-buttons');
+
+        this.addEventListeners()
+    }
+
+    addEventListeners() {
+        document.body.addEventListener('click', e => {
+            this.handleFormClick(e)
+        });
+    }
+
+        handleFormClick(e) {
+        const isFormClicked = this.$form.contains(e.target)
+
+        isFormClicked ? this.openForm() : this.closeForm()
+    }
+
+        openForm() {
+        this.$form.classList.add('form-open')
+        this.$noteTitle.style.display = 'block'
+        this.$formButtons.style.display = 'block'
+    }
+
+        closeForm() {
+        this.$form.classList.remove('form-open')
+        this.$noteTitle.style.display = 'none'
+        this.$formButtons.style.display = 'none'
     }
 }
+
+
 
 new App()
