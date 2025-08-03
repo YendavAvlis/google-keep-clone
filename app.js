5b189e2bd434b4caa3731f0ea772781ff8ad12ab
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-03 15:46:04 +0200

    feat: Add Notes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class App {
     constructor() {
+        this.notes = []
+
         this.$form = document.querySelector('#form');
         this.$noteTitle = document.querySelector('#note-title');
+        this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
 
         this.addEventListeners()
@@ -11,6 +14,23 @@ class App {
         document.body.addEventListener('click', e => {
             this.handleFormClick(e)
         });
+
+        this.$form.addEventListener('submit', e => {
+            e.preventDefault()
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+
+            const hasNote = title || text;
+
+            if (hasNote){
+                this.addNote({
+                    title: title,
+                    text: text
+                })
+            }
+
+
+        })
     }
 
         handleFormClick(e) {
@@ -29,6 +49,17 @@ class App {
         this.$form.classList.remove('form-open')
         this.$noteTitle.style.display = 'none'
         this.$formButtons.style.display = 'none'
+    }
+
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1: 1
+        };
+        this.notes = [...this.notes, newNote];
+        console.log(this.notes)
     }
 }
 
